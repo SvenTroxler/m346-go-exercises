b383b36a197b4af5a3d1e1e195026104bf0f4b1f
SvenTroxler <sventroxler1@gmail.com>
2025-11-03 15:22:13 +0100

    Startt Aufgabe 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Sven"
+	lastName := "Troxler"
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
