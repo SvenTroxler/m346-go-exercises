d99a743022343dec7132a04f92c4974af571071f
Sven Troxler <sventroxler1@gmail.com>
2025-11-10 11:06:53 +0100

    Update main.go

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,12 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
-	var firstName string = "Sven"
+	firstName := "Sven"
 	lastName := "Troxler"
-
+	dayOfBirth := 2
+	monthOfBirth := 3
+	yearOfBirth := 2009
+	numberOfSiblings := 2
+	heightInMeters := 1.74
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
 	fmt.Printf("Grösse (in Metern): %.2f\n", heightInMeters)
-	fmt.Printf("Sternzeichen: %c\n", zodiacSign)
 }
